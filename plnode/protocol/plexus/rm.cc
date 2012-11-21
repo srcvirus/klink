a038882b1ed5c1f99b5e78ec9aa75dc8bc43c6a5
faizulbari <faizulbari@gmail.com>
2012-11-20 19:22:16 -0500

    modified:   ../rm.cc

--- plnode/protocol/plexus/rm.cc
+++ plnode/protocol/plexus/rm.cc
@@ -16,12 +16,24 @@ rm::rm(int r, int m){
 }
 
 int* rm::encode(int* message){
+	if(strlen(message) != rm->k){
+		#ifdef DEBUG_MESSAGE
+		cout << "ERROR in rm.cc: rm-k = " << rm->k << " but message length " << strlen(message) << "."
+		#endif
+		return NULL;
+	}
 	int* codeword = new int[rm->n];
 	reedmuller_encode(rm, message, codeword);
 	return codeword;
 }
 
 int* rm::decode(int* codeword){
+	if(strlen(codeword) != rm->n){
+		#ifdef DEBUG_MESSAGE
+		cout << "ERROR in rm.cc: rm-n = " << rm->n << " but codeword length " << strlen(codeword) << "."
+		#endif
+		return NULL;
+	}
 	int* message = new int[rm->k];
 	reedmuller_decode(rm, codeword, message);
 	return message;
