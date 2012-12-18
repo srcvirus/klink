f15ecc72579f43773ff6f65b0dd8661a5d336087
Faiz <faizulbari@gmail.com>
2012-12-18 14:54:48 -0500

    modified:   plnode/ds/GlobalData.h

--- plnode/ds/GlobalData.h
+++ plnode/ds/GlobalData.h
@@ -19,6 +19,6 @@ public:
 };
 
 ReedMuller* GlobalData::rm = new ReedMuller(2, 4);
-string GlobalData::host_file_name = "pl_hostlist";
+string GlobalData::host_file_name = "ihostlist";
 int GlobalData::network_size;
 #endif /* GLOBALDATA_H_ */
