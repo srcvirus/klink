74b61d69de14939f0e21888a6232806e501f18dc
Faiz <faizulbari@gmail.com>
2012-12-20 22:59:47 -0500

    new file:   plnode/message/control/peer_dyn_change_status_message.h 	new file:   plnode/message/control/peer_gen_name_message.h

---
+++ plnode/message/control/peer_gen_name_message.h
@@ -0,0 +1,44 @@
+#ifndef PEER_GEN_NAME_MESSAGE_H
+#define PEER_GEN_NAME_MESSAGE_H
+
+#include "../message.h"
+
+#include <cstring>
+
+class PeerGenNameMessage: public ABSMessage
+{
+public:
+
+	PeerGenNameMessage()
+	{
+	}
+
+	PeerGenNameMessage(string source_host, int source_port, string dest_host,
+			int dest_port, OverlayID src_oid, OverlayID dst_id) :
+			ABSMessage(MSG_GENERATE_NAME, source_host, source_port, dest_host,
+					dest_port, src_oid, dst_id)
+	{
+	}
+
+	int getSize()
+	{
+		int ret = getBaseSize();
+		return ret;
+	}
+
+	virtual char* serialize(int* serialize_length)
+	{
+		int parent_length = 0;
+		char* parent_buffer = ABSMessage::serialize(&parent_length);
+		*serialize_length = parent_length;
+		return parent_buffer;
+	}
+
+	virtual ABSMessage* deserialize(char* buffer, int buffer_length)
+	{
+		ABSMessage::deserialize(buffer, buffer_length);
+		return this;
+	}
+};
+
+#endif
