7262868b179a638019fec9580921b3c2fa95611b
Shihab <sr2chowd@uwaterloo.ca>
2012-12-21 21:17:22 -0500

    modified:   .gitignore

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-if [ -f agent ]
+if [ -f pweb_agent ]
 then
 	rm agent
 fi
@@ -9,11 +9,11 @@ then
 	rm client
 fi
 
-g++ -g -static server_system_test.cc -lpthread -o agent &> server_output
-g++ -static client_system_test.cc -lpthread -o client &> client_output
+g++ -g -static server_system_test.cc -lpthread -o pweb_agent &> server_output
+g++ -g -static client_system_test.cc -lpthread -o client &> client_output
 grep error server_output
 grep error client_output
 rm server_output client_output
 
-scp config_temp agent ihostlist pweb@cn101.cs.uwaterloo.ca:~/klink
-ssh pweb@cn101.cs.uwaterloo.ca "cd klink; ./copy_script.sh"
+scp config pweb_agent ihostlist pweb@cn101.cs.uwaterloo.ca:~/klink
+ssh pweb@cn101.cs.uwaterloo.ca "cd klink; ./copy_script_src.sh"
