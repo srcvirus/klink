75bd32d7521fffb5499adaba1641265252945da9
Faiz <faizulbari@gmail.com>
2012-12-23 18:08:45 -0500

    modified:   build_deploy.sh 	modified:   client_system_test.cc 	modified:   make_agent_client.sh 	modified:   server_system_test.cc

--- build_deploy.sh
+++ build_deploy.sh
@@ -9,7 +9,7 @@ then
 	rm client
 fi
 
-g++ -g -static server_system_test.cc ./webinterface/mongoose.c ./plnode/protocol/plexus/goley/golay.c -lpthread -ldl -o agent &> server_output
+g++ -g -static server_system_test.cc ./webinterface/mongoose.c ./plnode/protocol/plexus/golay/golay.c -lpthread -ldl -o agent &> server_output
 g++ -g -static client_system_test.cc ./plnode/protocol/plexus/golay/golay.c -lpthread -o client &> client_output
 grep error server_output
 grep error client_output
