49dc7113e318e7f652dfc94c64974738d78bf257
Faiz <faizulbari@gmail.com>
2012-12-23 18:06:15 -0500

    modified:   build_deploy.sh 	modified:   make_agent_client.sh

--- make_agent_client.sh
+++ make_agent_client.sh
@@ -9,7 +9,7 @@ then
 fi
 g++ -static -g -W server_system_test.cc ./webinterface/mongoose.c ./plnode/protocol/plexus/goley/golay.c -lpthread -ldl -o agent &> output_agent
 grep error output_agent
-g++ -static -g -W client_system_test.cc ./plnode/protocol/plexus/goley/golay.c -lpthread -o client &> output_client
+g++ -static -g -W client_system_test.cc ./plnode/protocol/plexus/golay/golay.c -lpthread -o client &> output_client
 grep error output_client
 rm output_*
 
