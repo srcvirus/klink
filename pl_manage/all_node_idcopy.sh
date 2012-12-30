0db62d6fb24ba6a8ac9df36fe6596e893355033b
Shihab <sr2chowd@uwaterloo.ca>
2012-12-30 13:45:50 -0500

    modified:   pl_manage/all_node_idcopy.sh 	modified:   pl_manage/all_node_idgen.sh

--- pl_manage/all_node_idcopy.sh
+++ pl_manage/all_node_idcopy.sh
@@ -3,15 +3,11 @@ then
 	rm -r pssh_keys
 fi
 
-parallel-ssh -h pssh_nodes -x "-l uwaterloo_pweb" -x "-i ~/.ssh/id_rsa" -o pssh_keys/ "cat ~/.ssh/id_rsa.pub"
+parallel-ssh -h not_ready -x "-l uwaterloo_pweb" -x "-i ~/.ssh/id_rsa" -x "-o StrictHostKeyChecking=no" -o pssh_keys/ "cat ~/.ssh/id_rsa.pub"
 cat pssh_keys/* > pssh_keys/keys
 
 for i in 101 102 104 105 106 107 108 109 110
 do
-	if [ $i -eq 103 ]
-	then
-		continue
-	fi
 	host=cn$i.cs.uwaterloo.ca
 	cat pssh_keys/keys | ssh pweb@$host "cat >> ~/.ssh/authorized_keys"
 done
--- pl_manage/all_node_idgen.sh
+++ pl_manage/all_node_idgen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for host in `cat pssh_nodes`
+for host in `cat not_ready`
 do
 	./single_node_idgen.sh $host
 done
