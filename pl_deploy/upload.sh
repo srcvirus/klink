cf728b2c80dad11e1cee9ab64c9c058cfc256ff0
shihab <sr2chowd@ubuntu.ubuntu-domain>
2013-01-01 21:57:44 -0500

    new file:   pl_deploy/all_node_idcopy.sh 	new file:   pl_deploy/all_node_idgen.sh 	new file:   pl_deploy/find_non_ready_nodes.sh 	new file:   pl_deploy/pssh_nodes 	new file:   pl_deploy/ready_nodes 	new file:   pl_deploy/setup.sh 	new file:   pl_deploy/single_node_idgen.sh 	new file:   pl_deploy/upload.sh

---
+++ pl_deploy/upload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+start=$1
+n=$2
+end=$(($start+$n-1))
+#echo $end
+node_seg=`head -n $end pssh_nodes | tail -n $n`
+log_file="uplog_$start_$end"
+if [ -f $log_file ]
+then
+	rm $log_file
+fi
+#echo $node_seg
+for machine in $node_seg
+do
+	echo $machine &>> $log_file
+	scp -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no nodes imonitorlist agent config uwaterloo_pweb@$machine:~/ &>> $log_file
+	done=$(($done + 1))
+	echo "($done/$n) complete" &>> $log_file
+done
+
