e610f19c4ae42c596ee21b049502847da51a45c0
uwaterloo_pweb <sr2chowd@uwaterloo.ca>
2013-01-03 08:23:56 +0000

    new file:   agent

--- make_agent_client.sh
+++ make_agent_client.sh
@@ -7,10 +7,8 @@ if [ -f client ]
 then
 	rm client
 fi
-g++ -g -static -W server_system_test.cc ./webinterface/mongoose.c ./plnode/protocol/plexus/golay/golay.c -lpthread -ldl -o agent &> output_agent
+g++ -g -W server_system_test.cc ./webinterface/mongoose.c ./plnode/protocol/plexus/golay/golay.c -lpthread -ldl -o agent &> output_agent
 grep error output_agent
-g++ -static -g -W client_system_test.cc ./plnode/protocol/plexus/golay/golay.c -lpthread -o client &> output_client
+g++ -g -W client_system_test.cc ./plnode/protocol/plexus/golay/golay.c -lpthread -o client &> output_client
 grep error output_client
 rm output_*
-
-
