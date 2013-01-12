dd19625b9458e7bb7dd917df0ec9903fbc293a8e
Faiz <faizulbari@gmail.com>
2013-01-12 12:01:19 -0500

    modified:   plnode/protocol/code.h

--- plnode/protocol/code.h
+++ plnode/protocol/code.h
@@ -8,15 +8,30 @@
 #ifndef CODE_H_
 #define CODE_H_
 
-class ABSCode
-{
+class ABSCode {
 public:
-	ABSCode(){};
-	virtual ~ABSCode(){};
-	virtual long encode(long message) = 0;
-	virtual long decode(long codeword) = 0;
-        virtual int K() = 0;
-        virtual int N() = 0;
+
+        ABSCode() {
+        };
+
+        virtual ~ABSCode() {
+        };
+
+        virtual long encode(long message) {
+                return message;
+        }
+
+        virtual long decode(long codeword) {
+                return codeword;
+        }
+
+        virtual int K() {
+                return 20;
+        };
+
+        virtual int N() {
+                K();
+        };
 };
 
 #endif /* CODE_H_ */
