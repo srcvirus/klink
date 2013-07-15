03f919a7e64ce45b13aaa2ad871d9a31fe02f278
Faizul Bari <pweb@cn101.cs.uwaterloo.ca>
2013-07-15 11:33:06 -0400

    modified:   cdeploy.sh 	modified:   pweb.sqlite

--- cdeploy.sh
+++ cdeploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for(( i=101; i<=110; i++  ))
 do
-	scp agent pweb.sqlite config chostlist imonitorlist pweb@cn$i.cs.uwaterloo.ca:~/pweb_demo/
+	scp agent config chostlist imonitorlist pweb@cn$i.cs.uwaterloo.ca:~/pweb_demo/
 done
