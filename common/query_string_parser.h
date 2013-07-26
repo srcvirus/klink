5708dd2f9593f1a9e9501f9a16f78aee17517886
Faizul Bari <mfbari@uwaterloo.ca>
2013-07-26 08:57:42 -0400

    modified:   common/query_string_parser.h

--- common/query_string_parser.h
+++ common/query_string_parser.h
@@ -20,7 +20,12 @@ public:
 		vector<string> kvps = split(query_string, '&');
 		for(vector<string>::iterator it = kvps.begin(); it != kvps.end(); ++it) {
 			vector<string> kv = split(*it, '=');
-			key_value_store->add(urlDecode(kv[0]), urlDecode(kv[1]));
+			if(kv.size() == 2){
+				key_value_store->add(urlDecode(kv[0]), urlDecode(kv[1]));
+			}
+			else if(kv.size() == 1){
+				key_value_store->add(urlDecode(kv[0]), "");
+			} 
 			//printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
 			//printf(">>>>>>>>>%s, %s --> %s, %s<<<<<<<<<<<\n", kv[0].c_str(), kv[1].c_str(), urlDecode(kv[0]).c_str(), urlDecode(kv[1]).c_str());
 			//printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
@@ -32,19 +37,19 @@ public:
 	}
 	
 	vector<string> &split(const string &s, char delim, vector<string> &elems) {
-		printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
-		printf("Received: %s\n", s.c_str());
+		//printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+		//printf("Received: %s\n", s.c_str());
 		stringstream ss(s);
 		ss << std::noskipws;
 		string item;
 		while(getline(ss, item, delim)) {
 			elems.push_back(item);
 			
-			printf("item: %s\n", item.c_str());
+			//printf("item: %s\n", item.c_str());
 			
 			item.clear();
 		}
-		printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+		//printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
 		return elems;
 	}
 
