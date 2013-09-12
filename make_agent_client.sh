e0eee7982bf753756210212bacc5c0ce59ae17d2
srcvirus <shihab.buet@gmail.com>
2013-09-12 23:20:24 +0600

    Update make_agent_client.sh

--- make_agent_client.sh
+++ make_agent_client.sh
@@ -7,8 +7,8 @@ if [ -f client ]
 then
 	rm client
 fi
-g++ -g -W server_system_test.cc ./webinterface/mongoose.c ./plnode/protocol/plexus/golay/golay.c ./plnode/db/database.cc -lpthread -lsqlite3 -ldl -o agent &> output_agent
+g++ -g -W server.cc ./webinterface/mongoose.c ./plnode/protocol/plexus/golay/golay.c ./plnode/db/database.cc -lpthread -lsqlite3 -ldl -o agent &> output_agent
 grep error output_agent
-g++ -g -W client_system_test.cc ./plnode/protocol/plexus/golay/golay.c -lpthread -o client &> output_client
+g++ -g -W client.cc ./plnode/protocol/plexus/golay/golay.c -lpthread -o client &> output_client
 grep error output_client
 rm output_*
